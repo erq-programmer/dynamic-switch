8e1f6058c7f11b60b0fc286714adca5aec6f48ce
Eryk Slocinski <eryk.slocinski@gmail.com>
2020-08-12 23:56:49 +0200

    Added tests

--- src/dynamicSwitch.spec.ts
+++ src/dynamicSwitch.spec.ts
@@ -1,5 +1,34 @@
-// describe("isValid method", () => {
-//   test("it should return this.conditions", () => {
+export {}; // if not - error TS2451: Cannot redeclare block-scoped variable 'Switch'.
+const Switch = require("./dynamicSwitch");
+
+describe("Switch class under test", () => {
+
+    const checker = new Switch();
+    const callback = jest.fn();
+
+    it("isValid return true when all conditions is false", () => {
+        checker.add(false, callback);
+        checker.add(false, callback);
+        expect(checker.isValid()).toBe(true)
+    })
+
+    it("isValid return false when any conditions is true", () => {
+        checker.add(false, callback);
+        checker.add(false, callback);
+        checker.add(true, callback);
+        expect(checker.isValid()).toBe(false);
+    })
+
+    it("clears cases and conditions when isValid is done", () => {
+        expect(checker.cases).toHaveLength(0);
+        expect(checker.conditions).toHaveLength(0);
+
+    })
     
-//   })
-// })
+    it("when add 3 conditions by add method, it must be 3 length array with callbacks", () => {
+        checker.add(false, callback);
+        checker.add(false, callback);
+        checker.add(true, callback);
+        expect(checker.conditions).toHaveLength(3);
+    }) 
+})
